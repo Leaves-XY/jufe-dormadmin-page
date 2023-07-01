a460060c31575fb52688a14e31de13b28aed52bd
Leaves_XY <1479623723@qq.com>
2023-07-01 21:00:14 +0800

    add:添加通知和报修

---
+++ src/views/notice/NoticeInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoticeInfo"
+}
+</script>
+
+<style scoped>
+
+</style>
